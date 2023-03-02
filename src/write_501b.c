0b57b2554fa22f706f081d487598d289dea09455
Jack Humphries <jackphumphries02@gmail.com>
2023-03-02 16:02:03 +0000

    Added 501 byte versions

---
+++ src/write_501b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    char source[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit maximus ante non tristique. Suspendisse potenti. Proin gravida urna enim. Mauris nec purus risus. Nullam non ante purus. Pellentesque tempus scelerisque ipsum, a dignissim sapien facilisis nec. Aliquam aliquet metus nibh, sed mattis nisl vehicula a. Sed eleifend nisi vitae magna eleifend tincidunt. Praesent viverra velit nunc, vitae cursus ipsum luctus in. Sed in mattis nisi, quis elementum nibh. Integer ullamcorper pharetra.";
+
+    int len = strlen(source);
+    for (int i = 0; i < len; i++) {
+        write(1, &source[i], 1);
+    }
+    
+    printf("\n");
+}
